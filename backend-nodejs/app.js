6126fef9991e4ed511f49aeeb0530ea3ae4ce5a7
joonaskinnunen <joonas.kinn@gmail.com>
2021-09-26 23:56:16 +0300

    deploy

--- backend-nodejs/app.js
+++ backend-nodejs/app.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static('build'))
 
 app.use('/api/temperatures', temperatureRouter)
 app.use('/api/lights', lightRouter)
